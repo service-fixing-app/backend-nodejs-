f5ddc698b1d7f45e32b35f76b4597fbc69d401e5
khamsyher <114057321+khamsyher@users.noreply.github.com>
2023-10-25 14:16:52 +0700

    update code

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -1,8 +1,6 @@
 const User = require('../Models/userModel');
 
 const signup = async (req, res) => {
-//   const { fullname, email, password } = req.body;
-
   try {
     // Create a new user in the database
     const data = {
@@ -18,7 +16,67 @@ const signup = async (req, res) => {
     return res.status(400).json({ message: 'ລົງທະບຽນຜູ້ໃຊ້ງານຜິດພາດ' });
   }
 };
+// get all users
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll(); 
+    return res.status(200).json(users);
+  } catch (error) {
+    console.error('Error while fetching all users:', error);
+    return res.status(500).json({ message: 'Failed to retrieve users' });
+  }
+};
+
+
+//get user one user
+const getOneUser = async (req, res) => {
+  try {
+    const id = req.params.id; 
+
+    // Find the user by ID in the database
+    const user = await User.findOne({ where: { id } });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error('Error while fetching one user:', error);
+    return res.status(500).json({ message: 'Failed to retrieve user' });
+  }
+};
+
+
+// edit user
+const editUser = async (req, res) => {
+  try {
+    const id = req.params.id; 
+
+    // Find the user by ID in the database
+    const user = await User.findOne({ where: { id } });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    // Update user information with the data from the request body
+    user.fullname = req.body.fullname || user.fullname;
+    user.email = req.body.email || user.email;
+    user.password = req.body.password || user.password;
+
+    await user.save();
+
+    return res.status(200).json({ message: 'User updated successfully', user });
+  } catch (error) {
+    console.error('Error while updating user:', error);
+    return res.status(400).json({ message: 'Failed to update user' });
+  }
+};
+
 
 module.exports = {
     signup,
+    getAllUsers,
+    getOneUser,
+    editUser
 }
--- src/Routes/userRouter.js
+++ src/Routes/userRouter.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const userController = require('../Controllers/userController');
 
 router.post('/signup', userController.signup);
+router.put('/editUser/:id', userController.editUser);
+router.get('/getAllUsers', userController.getAllUsers);
+router.get('/getOneUser/:id', userController.getOneUser);
 
 module.exports = router;
